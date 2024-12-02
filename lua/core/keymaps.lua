7db2b9c5fd919aee0804e2d9ca05d0217dfa6378
sudarshanbashyal <sudarshanbsyl@gmail.com>
2024-12-02 23:42:25 +0545

    Setup noise.nvim

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -67,3 +67,6 @@ vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous dia
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+-- noise
+vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", {})
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -10,10 +10,36 @@ return {
 		},
 	},
 	{
+		"hrsh7th/cmp-cmdline",
+	},
+	{
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
+
+			-- `/` cmdline setup.
+			cmp.setup.cmdline("/", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+
+			-- `:` cmdline setup.
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{
+						name = "cmdline",
+						option = {
+							ignore_cmds = { "Man", "!" },
+						},
+					},
+				}),
+			})
 
 			cmp.setup({
 				snippet = {
