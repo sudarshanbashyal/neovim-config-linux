26cbe968934cf577bf55eb37618785f8278bae55
sudarshanbashyal <sudarshanbsyl@gmail.com>
2024-12-04 21:48:18 +0545

    add smear cursor plugin

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls", "vtsls", "prettier" },
+				ensure_installed = { "lua_ls", "gopls", "vtsls" },
 				auto_install = true,
 			})
 		end,
@@ -42,7 +42,7 @@ return {
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
 		end,
 	},
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -112,4 +112,26 @@ return {
 			})
 		end,
 	},
+	{
+		"sphamba/smear-cursor.nvim",
+
+		opts = {
+			-- Smear cursor color. Defaults to Cursor GUI color if not set.
+			-- Set to "none" to match the text color at the target cursor position.
+			cursor_color = "#c9ad7d",
+
+			-- Background color. Defaults to Normal GUI background color if not set.
+			normal_bg = "#282828",
+
+			-- Smear cursor when switching buffers or windows.
+			smear_between_buffers = true,
+
+			-- Smear cursor when moving within line or to neighbor lines.
+			smear_between_neighbor_lines = true,
+
+			-- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+			-- Smears will blend better on all backgrounds.
+			legacy_computing_symbols_support = false,
+		},
+	},
 }
